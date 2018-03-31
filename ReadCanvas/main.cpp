8aa4639cfb70a1d5d7cd858d9c3e9031f51d803e
InsulinShock <gmcvitti@gmail.com>
2018-03-31 09:50:40 -0400

    Created setup script and added intellisense for emscripten

---
+++ ReadCanvas/main.cpp
@@ -0,0 +1,7 @@
+#include <emscripten.h>
+#include <vector>
+
+int main() {
+
+    return 0;
+}
---
+++ setup.bat
@@ -0,0 +1,5 @@
+:: Get Emscripten
+call git clone https://github.com/juj/emsdk.git
+call .\emsdk\emsdk.bat install latest
+call .\emsdk\emsdk.bat activate latest 
+call emcc --version
